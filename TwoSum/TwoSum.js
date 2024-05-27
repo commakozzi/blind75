039cf92b87f416a8555327989b3e11a2a9a93e4e
commakozzi <commakozzi1@gmail.com>
2024-05-27 01:33:50 +0000

    Update

--- TwoSum/TwoSum.js
+++ TwoSum/TwoSum.js
@@ -4,6 +4,10 @@ var result = [];
 
 TwoSum(nums, target) {
   for (var i = 0; i < nums.length; i++) {
-    
+    for (var j = i+1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        result += [nums[i], nums[j]];
+    }
+    return result;
   }
 }
