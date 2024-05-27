c49430b4ffca482a2ba5d54118e66083a687cd08
commakozzi <commakozzi1@gmail.com>
2024-05-27 16:48:34 -0400

    Update TwoSum.js

--- TwoSum/TwoSum.js
+++ TwoSum/TwoSum.js
@@ -1,3 +1,23 @@
+/* My solution */
+
+var twoSum = function(nums, target) {
+    var firstIndex = 0;
+    var lastIndex = 0;
+    var result = [];
+    
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                firstIndex = i;
+                lastIndex = j;
+                result = [firstIndex, lastIndex];
+            }
+        }
+    }
+    
+    return result;
+};
+
 /* Hash map example from ChatGPT (4o) */
 
 var twoSum = function(nums, target) {
