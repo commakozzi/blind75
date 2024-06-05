57370fa63aa2437be19bacc431bb6d06f6d4a5eb
Michael Causey <commakozzi1@gmail.com>
2024-06-05 01:09:43 -0400

    Update mergeAlternately.js

--- MergeAlternately/mergeAlternately.js
+++ MergeAlternately/mergeAlternately.js
@@ -21,16 +21,17 @@ var mergeAlternately = function(word1, word2) {
   };
 
 /* ----------------------------------------------------------------------
-Example from ChatGPT (4o) 
+Example from ChatGPT (4o) but suggested by my solution.  This is not 
+the best way to do this at all.  Why convert to array when you can 
+just use the string methods?  I was overthinking this one.
 -------------------------------------------------------------------------*/
 
 var mergeAlternately = function(word1, word2) {
     let word1Array = word1.split(''); // single or double quotes here are required.
     let word2Array = word2.split(''); // single or double quotes here are required.
     let resultingArray = [];
-    let maxLength = Math.max(word1Array.length, word2Array.length);
 
-    for (let i = 0; i < maxLength; i++) {
+    for (let i = 0; i < Math.max(word1Array.length, word2Array.length); i++) {
         if (word1Array[i]) {
             resultingArray.push(word1Array[i]);
         }
@@ -41,3 +42,18 @@ var mergeAlternately = function(word1, word2) {
 
     return resultingArray.join(''); // single or double quotes here are required.
 }
+
+/* ----------------------------------------------------------------------
+Best answer from Leetcode.  this example was important for me to check 
+runtime on.  The top answer got 38ms runtime.  My answer with spaces
+between the lines got 60ms plus.  when i took the spaces out i got 42ms.
+-------------------------------------------------------------------------*/
+
+var mergeAlternately = function(word1, word2) {
+  let results = '';
+  for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+      if (i < word1.length) results += word1[i];
+      if (i < word2.length) results += word2[i];
+  }
+  return results;
+};
