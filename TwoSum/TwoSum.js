fc580115ecabcdf548aa9cc829bd4bbac1723fcb
commakozzi <commakozzi1@gmail.com>
2024-06-13 09:52:10 -0400

    Update TwoSum.js

--- TwoSum/TwoSum.js
+++ TwoSum/TwoSum.js
@@ -3,10 +3,15 @@ My solution
 -------------------------------------------------------------------------*/
 
 var twoSum = function(nums, target) {
+    // Create variables to hold first index, last index and the result:
     var firstIndex = 0;
     var lastIndex = 0;
     var result = [];
     
+    /* Loop through the nums array and compare each number with the next until
+    	   two numbers are found that add to the target and set the first number
+       to the firstIndex variable and set the second number to the lastIndex
+       variable.  Finally, add these to the result array. */
     for (var i = 0; i < nums.length; i++) {
         for (var j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] == target) {
@@ -17,6 +22,7 @@ var twoSum = function(nums, target) {
         }
     }
     
+    // return the result array
     return result;
 };
 
@@ -25,8 +31,13 @@ Hash map example from ChatGPT (4o)
 -------------------------------------------------------------------------*/
 
 var twoSum = function(nums, target) {
+    // Create an empty hash map to store the index of the nums that add up to the target.
     let map = new Map();
     
+    /* Loop through the nums array and find the number that when subtracted from
+       the target, equals the target when added to another number in the nums array.  
+       Do this by saving the complement to the hash map and then checking whether 
+       that number exists when the next nums is subtracted from the target. */
     for (let i = 0; i < nums.length; i++) {
         let complement = target - nums[i];
         
